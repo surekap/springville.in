7005b4829163c2570171436351de96975a2d59b1
Rajan <rajan@surekaproperties.com>
2019-08-27 15:27:10 +0530

    Parse data from Bitbucket hook payload

--- 5d5m82y.php
+++ 5d5m82y.php
@@ -10,7 +10,8 @@ $git_bin_path = 'git';
 $update = true;
 
 // Parse data from Bitbucket hook payload
-$payload = json_decode($_POST['payload']);
+//$payload = json_decode($_POST['payload']);
+$payload = json_decode($_REQUEST['payload']);
 
 if (empty($payload->commits)){
   // When merging and pushing to bitbucket, the commits array will be empty.
