0741fe057460a0e294a81b583b5e0d6a5979b1b2
Rajan <rajan@surekaproperties.com>
2019-12-10 15:36:59 +0530

    testing

--- 5d5m82y_backup.php
+++ 5d5m82y_backup.php
@@ -7,16 +7,16 @@ $web_root_dir = '/var/www/' . SITE . '/public';
 // Full path to git binary is required if git is not in your PHP user's path. Otherwise just use 'git'.
 $git_bin_path = 'git';
 
-$update = false;
+$update = true;
 
 // Parse data from Bitbucket hook payload
-$payload = json_decode($_POST['payload'] ? "[]");
+$payload = json_decode($_POST['payload']);
+
 
 if (empty($payload->commits)){
   // When merging and pushing to bitbucket, the commits array will be empty.
   // In this case there is no way to know what branch was pushed to, so we will do an update.
   $update = true;
-  $branch = "master";
 } else {
   foreach ($payload->commits as $commit) {
     $branch = $commit->branch;
@@ -36,5 +36,4 @@ if ($update) {
   $commit_hash = shell_exec('cd ' . $repo_dir . ' && ' . $git_bin_path  . ' rev-parse --short HEAD');
   file_put_contents('deploy.log', date('m/d/Y h:i:s a') . " Deployed branch: " .  $branch . " Commit: " . $commit_hash . "\n", FILE_APPEND);
 }
-
 ?>
